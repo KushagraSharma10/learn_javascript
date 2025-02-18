3179fb786f49d3a3d0f14f84bac628f4b55afdfd
KushagraSharma10 <sharmakartik3888@gmail.com>
2025-02-19 00:52:35 +0530

    a seperate js file is created for both tasks

---
+++ 17-02-2025/starPattern/starPattern.js
@@ -0,0 +1,31 @@
+index = document
+        .getElementById("number")
+        .addEventListener("input", function () {
+          index = this.value;
+        });
+
+      function printStars(index) {
+        if(index <= 0){
+            return "Please enter a positive integer greater than 0."
+        }
+
+        let result = ""
+        for (let i = 1; i <= index; i++) {
+          let str = "";
+          for (let j = 1; j <= i; j++) {
+            str += "* ";
+          }
+          console.log(str);
+          result += str + "<br>";
+        }
+        return result
+      }
+
+      document.getElementById("submit").addEventListener("click", function () {
+        document.getElementById("result").innerHTML =  printStars(index);
+      });
+
+      document.getElementById("clear").addEventListener("click", function () {
+        document.getElementById("number").value = "";
+        document.getElementById("result").innerHTML = "";
+      });
